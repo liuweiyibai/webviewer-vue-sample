e8b1ca839eeb66500645d52687894fdc0b27e7ac
hotblin <lw1140@163.com>
2021-05-23 20:55:10 +0800

    update

--- src/components/WebViewer.vue
+++ src/components/WebViewer.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="webviewer" ref="viewer"></div>
+  <div id="webviewer" ref="viewer">
+    <a href="javascript:void(0);" class="save-btn" @click="handleSave"
+      >保存阅读进度</a
+    >
+  </div>
 </template>
 
 <script>
@@ -11,15 +15,34 @@ export default {
   props: { initialDoc: { type: String } },
   setup(props) {
     const viewer = ref(null);
+    const appInstance = ref(null);
     onMounted(() => {
       const path = `${process.env.BASE_URL}webviewer`;
-      WebViewer({ path, initialDoc: props.initialDoc }, viewer.value).then(instance=>{
-         instance.setLanguage('zh_cn');
-         instance.enableElements(['bookmarksPanel', 'bookmarksPanelButton']);
-      });
+      WebViewer({ path, initialDoc: props.initialDoc }, viewer.value).then(
+        (instance) => {
+          appInstance.value = instance;
+          const { docViewer, iframeWindow } = instance;
+          instance.setLanguage('zh_cn');
+          instance.enableElements(['bookmarksPanel', 'bookmarksPanelButton']);
+          iframeWindow.addEventListener('userBookmarksChanged', (e) => {
+            const bookmarks = e.detail;
+            const bookmarksString = JSON.stringify(bookmarks);
+            console.log(`添加书签::${JSON.stringify(bookmarksString)}`);
+          });
+
+          setTimeout(() => {
+            docViewer.setCurrentPage(20);
+          }, 2000);
+        }
+      );
     });
+
+    const handleSave = () => {
+      console.log(appInstance.value.getCurrentPageNumber());
+    };
     return {
       viewer,
+      handleSave,
     };
   },
 };
@@ -27,6 +50,14 @@ export default {
 
 <style>
 #webviewer {
-  height: 800px;
+  height: 100vh;
+}
+.save-btn {
+  z-index: 10000;
+  color: coral;
+  position: fixed;
+  bottom: 10px;
+  left: 20px;
+  text-decoration: none;
 }
 </style>
